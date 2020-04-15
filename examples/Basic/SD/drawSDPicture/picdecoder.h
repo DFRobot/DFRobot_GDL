af2b5bfce833dc2390ebf096c56c105eb138ae72
Frances9 <44355133+Frances9@users.noreply.github.com>
2020-04-15 17:01:14 +0800

    Update picdecoder.h

--- examples/Basic/SD/drawSDPicture/picdecoder.h
+++ examples/Basic/SD/drawSDPicture/picdecoder.h
@@ -1,5 +1,5 @@
 /**
- * @brief Can display 16bit 24bit and 32bit BMP and JFIF format JPEG / JPG pictures
+ * @brief Display 16bit 24bit and 32bit BMP and JFIF format JPEG / JPG pictures
  */
 #ifndef __PICDECODER_H__
 #define __PICDECODER_H__
@@ -74,53 +74,53 @@
 #define BMPTYPE 1
 #define JPGTYPE 2
 
-#define BI_RGB	 		0  //没有压缩.RGB 5,5,5. No compression. RGB 5, 5, 5.
-#define BI_RLE8 		1  //每个象素8比特的RLE压缩编码，压缩格式由2字节组成(重复象素计数和颜色索引)； 8-bit RLE compression coding for each pixel, the compression format consists of 2 bytes (repeated pixel count and color index);
-#define BI_RLE4 		2  //每个象素4比特的RLE压缩编码，压缩格式由2字节组成 4-bit RLE compression coding for each pixel, the compression format consists of 2 bytes
-#define BI_BITFIELDS 	3  //每个象素的比特由指定的掩码决定.  The bit of each pixel is determined by the specified mask.
+#define BI_RGB	 		0  //No compression. RGB 5, 5, 5.
+#define BI_RLE8 		1  //8-bit RLE compression for each pixel, the compression format consists of 2 bytes (repeated pixel count and color index);
+#define BI_RLE4 		2  //4-bit RLE compression for each pixel, the compression format consists of 2 bytes
+#define BI_BITFIELDS 	3  //The bit of each pixel is determined by the specified mask.
  
 //BMP信息头  BMP information header
 typedef  struct
 {
-  uint32_t biSize ;       //说明BITMAPINFOHEADER结构所需要的字数。 Explain the number of words required by the BITMAPINFOHEADER structure.
-  int32_t  biWidth ;              //说明图象的宽度，以象素为单位  Specifies the width of the image, in pixels
-  int32_t  biHeight ;             //说明图象的高度，以象素为单位  State the height of the image in pixels
-  uint16_t  biPlanes ;        //为目标设备说明位面数，其值将总是被设为1  Specify the number of planes for the target device, its value will always be set to 1
-  uint16_t  biBitCount ;      //说明比特数/象素，其值为1、4、8、16、24、或32   Specifies the number of bits / pixel, the value is 1, 4, 8, 16, 24, or 32
-  uint32_t biCompression ;//说明图象数据压缩的类型。其值可以是下述值之一：   Explain the type of image data compression. Its value can be one of the following values:
-  uint32_t biSizeImage ;  //说明图象的大小，以字节为单位。当用BI_RGB格式时，可设置为0  Specifies the size of the image, in bytes. When using BI_RGB format, it can be set to 0
-  int32_t  biXPelsPerMeter ;      //说明水平分辨率，用象素/米表示  Description of horizontal resolution, expressed in pixels / meter
-  int32_t  biYPelsPerMeter ;      //说明垂直分辨率，用象素/米表示  Description of vertical resolution, expressed in pixels / meter
-  uint32_t biClrUsed ;    //说明位图实际使用的彩色表中的颜色索引数  Explain the number of color indexes in the color table actually used by the bitmap
-  uint32_t biClrImportant ;//说明对图象显示有重要影响的颜色索引的数目，如果是0，表示都重要。Explain the number of color indexes that have an important influence on the image display. If it is 0, it means that all are important.
+  uint32_t biSize ;       //The number of words required by the BITMAPINFOHEADER structure.
+  int32_t  biWidth ;              //The width of the image, in pixels
+  int32_t  biHeight ;             //The height of the image, in pixels
+  uint16_t  biPlanes ;        //The number of planes for the target device, its value will always be set to 1
+  uint16_t  biBitCount ;      //The number of bits / pixel, could be 1, 4, 8, 16, 24, or 32
+  uint32_t biCompression ;//The compression type of image data. Its value can be one of the followings:
+  uint32_t biSizeImage ;  //The size of the image, in bytes. When using BI_RGB format, it can be set to 0
+  int32_t  biXPelsPerMeter ;      //The horizontal resolution, expressed in pixels / meter
+  int32_t  biYPelsPerMeter ;      //The vertical resolution, expressed in pixels / meter
+  uint32_t biClrUsed ;    //The number of color indexes in the color table actually used by the bitmap
+  uint32_t biClrImportant ;//The number of color indexes that has an important influence on the image display. If it is 0, it means that all are important.
 } __attribute__ ((packed)) BITMAPINFOHEADER ;
 
-//BMP头文件 BMP header file
+//BMP header file
 typedef  struct
 {
-  uint16_t  bfType ;        //文件标志.只对'BM',用来识别BMP位图类型  File logo. Only for 'BM', used to identify BMP bitmap type
-  uint32_t bfSize ;     //文件大小,占四个字节  File size, takes up four bytes
+  uint16_t  bfType ;        //File logo. Only for 'BM', used to identify BMP bitmap type
+  uint32_t bfSize ;     //File size, takes up four bytes
   uint16_t  bfReserved1 ;   //reserve
   uint16_t  bfReserved2 ;   //reserve
-  uint32_t bfOffBits ;  //从文件开始到位图数据(bitmap data)开始之间的的偏移量  Offset from the start of the file to the start of bitmap data
+  uint32_t bfOffBits ;  //Offset from the start of the file to the start of bitmap data
 } __attribute__ ((packed)) BITMAPFILEHEADER ;
 
-//彩色表  Color table
+// Color table
 // typedef  struct
 // {
-  // uint8_t rgbBlue ;    //指定蓝色强度  Specify blue intensity
-  // uint8_t rgbGreen ;   //指定绿色强度  Specify green intensity
-  // uint8_t rgbRed ;     //指定红色强度  Specify red intensity
+  // uint8_t rgbBlue ;    //Specify blue intensity
+  // uint8_t rgbGreen ;   //Specify green intensity
+  // uint8_t rgbRed ;     //Specify red intensity
   // uint8_t rgbReserved ;//reserve，Set to 0
 // } __attribute__ ((packed)) RGBQUAD ;
 // typedef RGBQUAD * LPRGBQUAD;
 
-//位图信息头  Bitmap information header
+//Bitmap information header
 typedef  struct
 {
   BITMAPFILEHEADER bmfHeader;
   BITMAPINFOHEADER bmiHeader;
-  //u32 RGB_MASK[3];     //调色板用于存放RGB掩码.  The palette is used to store RGB masks.
+  //u32 RGB_MASK[3];     //The palette is used to store RGB masks.
   //RGBQUAD bmiColors[256];
 } __attribute__ ((packed)) BITMAPINFO;
 
