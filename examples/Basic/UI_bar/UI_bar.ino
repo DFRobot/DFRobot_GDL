3faba5dd7edab69145f3c9f72702e1ddc245200c
Frances9 <44355133+Frances9@users.noreply.github.com>
2020-04-16 14:03:01 +0800

    Update UI_bar.ino

--- examples/Basic/UI_bar/UI_bar.ino
+++ examples/Basic/UI_bar/UI_bar.ino
@@ -1,7 +1,7 @@
 /*!
  * @file UI_bar.ino
- * @brief Create a progress bar control on the screen that allows the user to customize the parameters of the progress bar, or they can use the 
- * default parameters.
+ * @brief Create a progress bar control on the screen that allows the user to customize the parameters of the progress bar,
+ * @n or they can use the default parameters.
  * @n The user can control the value of the progress bar through the callback function of the progress bar.
  * @n The example supports Arduino Uno, Leonardo, Mega2560, FireBeetle-ESP32, FireBeetle-ESP8266, FireBeetle-M0.
  * @copyright  Copyright (c) 2010 DFRobot Co.Ltd (http://www.dfrobot.com)
@@ -27,7 +27,7 @@
 #define TFT_DC  D3
 #define TFT_CS  D4
 #define TFT_RST D5
-/*AVR系列主板*/
+/*AVR series mainboard*/
 #else
 #define TFT_DC  2
 #define TFT_CS  3
@@ -45,7 +45,7 @@
 //DFRobot_ST7789_240x320_HW_SPI screen(/*dc=*/TFT_DC,/*cs=*/TFT_CS,/*rst=*/TFT_RST);
 //DFRobot_ILI9341_240x320_HW_SPI  screen(/*dc=*/TFT_DC,/*cs=*/TFT_CS,/*rst=*/TFT_RST);
 //DFRobot_ILI9488_320x480_HW_SPI screen(/*dc=*/TFT_DC,/*cs=*/TFT_CS,/*rst=*/TFT_RST);
-/*M0主板下DMA传输*/ /* M0 motherboard DMA transfer */
+/* M0 mainboard DMA transfer */
 //DFRobot_ST7789_240x240_DMA_SPI screen(/*dc=*/TFT_DC,/*cs=*/TFT_CS,/*rst=*/TFT_RST);
 //DFRobot_ST7789_240x320_DMA_SPI screen(/*dc=*/TFT_DC,/*cs=*/TFT_CS,/*rst=*/TFT_RST);
 //DFRobot_ILI9341_240x320_DMA_SPI screen(/*dc=*/TFT_DC,/*cs=*/TFT_CS,/*rst=*/TFT_RST);
@@ -91,28 +91,28 @@ void setup()
 {
   
   Serial.begin(9600);
-  //ui初始化 ui initialization
+  //Initialize UI
   ui.begin();
   ui.setTheme(DFRobot_UI::MODERN);
 
-  //在屏幕上显示字符串  Display a string on the screen
+  //Display a string on the screen
   ui.drawString(/*x=*/33,/*y=*/screen.height()/5*4,"Page of loading",COLOR_RGB565_WHITE,ui.bgColor,/*fontsize =*/2,/*Invert=*/0);
-  //创建一个进度条控件  Create a progress bar control
+  //Create a progress bar control
   DFRobot_UI::sBar_t &bar1 = ui.creatBar();
-  /**用户自定义进度条参数*/ /** User-defined progress bar parameters **/
+  /** User-defined progress bar parameter **/
   bar1.setStyle(DFRobot_UI::COLUMN);
   bar1.fgColor = COLOR_RGB565_GREEN;
   bar1.setCallback(barCallback1);
   ui.draw(&bar1,/*x=*/33,/*y=*/screen.height()/5*3);
   
   DFRobot_UI::sBar_t &bar2 = ui.creatBar();
-  /**用户自定义进度条参数*/ /**User-defined progress bar parameters*/
+  /**User-defined progress bar parameter*/
   bar2.setStyle(DFRobot_UI::CIRCULAR);
   bar2.setCallback(barCallback2);
   ui.draw(&bar2,/*x=*/120,/*y=*/screen.height()/5*2);
 
   DFRobot_UI::sBar_t &bar3 = ui.creatBar();
-  /**用户自定义进度条参数*/ /**User-defined progress bar parameters*/
+  /**User-defined progress bar parameter*/
   bar3.fgColor = COLOR_RGB565_BLUE;
   bar3.setStyle(DFRobot_UI::BAR);
   bar3.setCallback(barCallback3);
