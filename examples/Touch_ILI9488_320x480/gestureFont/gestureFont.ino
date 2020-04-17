62f0f844e21476fcac74a7d6051ebb4587f964c8
Frances9 <44355133+Frances9@users.noreply.github.com>
2020-04-17 16:39:59 +0800

    Update gestureFont.ino

--- examples/Touch_ILI9488_320x480/gestureFont/gestureFont.ino
+++ examples/Touch_ILI9488_320x480/gestureFont/gestureFont.ino
@@ -1,20 +1,7 @@
 /*!
  * @file gettureFont.ino
- * @brief 屏幕中央存在几个文字，可以通过两指手势中的放大，缩小，上滑，下滑来控制它
- * @n 本示例支持的主板有Arduino Uno, Leonardo,Mega2560, FireBeetle-ESP32, FireBeetle-ESP8266, FireBeetle-M0
- *
- * @copyright  Copyright (c) 2010 DFRobot Co.Ltd (http://www.dfrobot.com)
- * @licence     The MIT License (MIT)
- * @author [fengli](li.feng@dfrobot.com)
- * @version  V1.0
- * @date  2019-12-6
- * @get from https://www.dfrobot.com
- * @url https://github.com/DFRobot/DFRobot_GDL/src/DFRpbot_UI
-*/
-/*!
- * @file gettureFont.ino
- * @brief There are several words in the center of the screen, which can be controlled by zooming in, zooming out, sliding up, and sliding down in a two-finger gesture
- * @n The motherboards supported by this example are Arduino Uno, Leonardo, Mega2560, FireBeetle-ESP32, FireBeetle-ESP8266, FireBeetle-M0
+ * @brief Control the words in the center of the screen by zooming in, zooming out, sliding up, and sliding down in a two-finger gesture.
+ * @n The demo supports Arduino Uno, Leonardo, Mega2560, FireBeetle-ESP32, FireBeetle-ESP8266, FireBeetle-M0
  *
  * @copyright Copyright (c) 2010 DFRobot Co. Ltd (http://www.dfrobot.com)
  * @licence The MIT License (MIT)
@@ -38,7 +25,7 @@
 #define TFT_DC  D3
 #define TFT_CS  D4
 #define TFT_RST D5
-/*AVR系列主板*/  /* AVR series motherboard */
+/* AVR series mainboard */
 #else
 #define TFT_DC  2
 #define TFT_CS  3
@@ -46,46 +33,36 @@
 #endif
 
 /**
-   @brief Constructor  当触摸采用gt系列芯片时，可以调用此构造函数 When the touch uses the gt series chip, you can call this constructor
+   @brief Constructor  When the touch uses the gt series chip, you can call this constructor
 */
 DFRobot_Touch_GT911 touch;
 
-/**
-   @brief Constructor  当屏采用硬件SPI通信，驱动IC是st7789，屏幕分辨率是240x320时，可以调用此构造函数
-   @param dc  SPI通信的命令/数据线引脚
-   @param cs  SPI通信的片选引脚
-   @param rst  屏的复位引脚
-*/
+
 /**
    @brief Constructor When the screen uses hardware SPI communication, the driver IC is st7789, and the screen resolution is 240x320, this constructor can be called
-   @param dc Command / data line pin for SPI communication
+   @param dc Command/data line pin for SPI communication
    @param cs Chip select pin for SPI communication
    @param rst Reset pin of the screen
 */
 DFRobot_ILI9488_320x480_HW_SPI screen(/*dc=*/TFT_DC,/*cs=*/TFT_CS,/*rst=*/TFT_RST);
-/*M0主板下DMA传输*/ /* M0 motherboard DMA transfer */
+/* M0 mainboard DMA transfer */
 //DFRobot_ILI9488_320x480_DMA_SPI screen(/*dc=*/TFT_DC,/*cs=*/TFT_CS,/*rst=*/TFT_RST);
 
 
 /**
-   @brief 构造函数
-   @param gdl 屏幕对象
-   @param touch 触摸对象
-*/
-/**
-   @brief constructor
-   @param gdl screen object
-   @param touch touch object
+   @brief Constructor
+   @param gdl Screen object
+   @param touch Touch object
 */
 DFRobot_UI ui(&screen, &touch);
 
-uint8_t fontSize = 2; //字体的大小 Font size
-uint16_t fontColor = 0xf800; //字体的颜色 Font color
-uint8_t fontLen = 4 * fontSize * 8; //字符串宽度 String width
-char * font = "DFRC"; //字符串 String
-uint8_t fontHeight = fontSize * 8;//字符串高度  String height
-uint16_t posx =  screen.width() / 2 ; //字符串起始位置的x坐标 The x coordinate of the starting position of the string
-uint16_t posy =  screen.height()  / 2;//字符串起始位置的y坐标 The y coordinate of the beginning of the string
+uint8_t fontSize = 2; //Font size
+uint16_t fontColor = 0xf800; //Font color
+uint8_t fontLen = 4 * fontSize * 8; //String width
+char * font = "DFRC"; // String
+uint8_t fontHeight = fontSize * 8;//String height
+uint16_t posx =  screen.width() / 2 ; //The x-coordinate of the starting position of the string
+uint16_t posy =  screen.height()  / 2;//the y-coordinate of the beginning of the string
 
 void setup()
 {
@@ -93,7 +70,7 @@ void setup()
   Serial.begin(9600);
   //UI initialization
   ui.begin();
-  //绘制最初的字符串 Draw the initial string
+  //Draw the initial string
   ui.drawString(posx - fontLen / 2-8, posy - fontHeight / 2, font, fontColor , ui.bgColor, fontSize, 0);
 
 
@@ -113,10 +90,10 @@ void refresh() {
     }                
   */
   switch (gesture) {
-	//检测到手势为缩小手势 Gesture detected as zoom out gesture
+	//Gesture detected as zoom out 
     case ui.SHRINK : {
         screen.fillRect(posx - fontLen / 2 -8,  posy - fontHeight / 2, 4 * fontSize * 8, fontSize * 8, ui.bgColor);
-        //fontSize减小  fontSize decreases
+        //fontSize decrease
 	     	fontSize--;
         if (fontSize <= 1) fontSize = 1;
 
@@ -124,17 +101,17 @@ void refresh() {
         fontLen = 4 * fontSize * 8;
         ui.drawString(posx - fontLen / 2-8 ,  posy - fontHeight / 2,  font, fontColor , ui.bgColor, fontSize, 0);
       }; break;
-	//检测到手势为放大手势 Gesture detected as zoom in gesture
+	//Gesture detected as zoom in 
     case ui.MAGNIFY : {
         screen.fillRect(posx - fontLen / 2-8 ,  posy - fontHeight / 2, 4 * fontSize * 8, fontSize * 8, ui.bgColor);
-        //fontSize增大  fontSize increase
+        //fontSize increase
 		    fontSize++;
         if (fontSize >= 7) fontSize = 7;
         fontHeight = fontSize * 8;
         fontLen = 4 * fontSize * 8;
         ui.drawString(posx - fontLen / 2-8 , posy - fontHeight / 2, font, fontColor , ui.bgColor, fontSize, 0);
       } break;
-    //检测到手势为两指向上滑动 Gesture detected swiping up with two fingers
+    //Gesture detected as swiping up with two fingers
     case ui.DUPGLIDE : {
         screen.fillRect(posx - fontLen / 2-8 ,   posy - fontHeight / 2, 4 * fontSize * 8, fontSize * 8, ui.bgColor);
         posy -= 10 ;
@@ -142,7 +119,7 @@ void refresh() {
         fontLen = 4 * fontSize * 8;
         ui.drawString(posx - fontLen / 2 - 8,  posy - fontHeight / 2, font, fontColor , ui.bgColor, fontSize, 0);
       } break;
-	//检测到手势为两指向下滑动 Gesture detected swiping down with two fingers
+	//Gesture detected as swiping down with two fingers
     case ui.DDOWNGLIDE : {
         screen.fillRect(posx - fontLen / 2 - 8,  posy - fontHeight / 2, 4 * fontSize * 8, fontSize * 8, ui.bgColor);
         posy += 10;
