7f10444fd64faa23d62b11a257186eb450b78815
Frances9 <44355133+Frances9@users.noreply.github.com>
2020-04-27 14:37:41 +0800

    Update DFRobot_Gesture.h

--- src/DFRobot_Gesture.h
+++ src/DFRobot_Gesture.h
@@ -88,127 +88,72 @@ typedef struct{
   	  UNABLE,
   }eDir_t;
   /**
-   * @brief 获取触摸屏检测到的手势
-   * @param str 字符串，里面包含了触摸点信息
-   * @return eGesture_t 类型的变量代表了不同的手势
-   */
-  /**
     * @brief Get the gesture detected by the touch screen
     * @param str String, which contains touch point information
-    * @return  Variables of eGesture_t type, represent different gestures
+    * @return  Variables of eGesture_t type represent different gestures
     */
   eGesture_t gesture(String str);
   /**
-   * @brief 获取触摸屏检测到的手势
-   * @param release 触摸的过程中手指是否松开
-   * @param number 触摸手指的个数
-   * @return eGesture_t 类型的变量代表了不同的手势
-   */
-  /**
     * @brief Get the gesture detected by the touch screen
-    * @param release Whether the finger was released during the touch
-    * @param number touch the number of fingers
-    * Variables of type @return eGesture_t represent different gestures
+    * @param release Whether the finger released during the touch
+    * @param number The number of fingers touching the screen
+    * @return Variables of eGesture_t type represent different gestures
     */
   eGesture_t fingers(uint8_t release,uint8_t number);
   /**
-   * @brief 计算从触摸开始第一手指的位置离第一个手指结束时的位置的距离
-   * @return 两个点之间的距离
-   */
-  /**
-    * @brief Calculate the distance from the position of the first finger from the beginning of the touch to the position of the end of the first finger
-    * @return the distance between two points
+    * @brief Calculate the distance from the touch beginning position of the first finger to the end position of the first finger. 
+    * @return Distance between two points
     */
   uint16_t clickCount();
   /**
-   * @brief 将字符串里面点的信息转化到数组内以备用
-   * @param str 字符串，里面包含了触摸点信息
-   * @return 触摸屏检测到触摸点的个数
-   */
-  /**
-    * @brief converts the information in the string into the array for backup
-    * @param str string, which contains touch point information
+    * @brief Convert the point information in the string into array for later use
+    * @param str String, which contains touch point information
     * @return The number of touch points detected by the touch screen
     */
   virtual uint8_t stringToPoint(String str)=0;
   
   /**
-   * @brief 当只有单个手指触摸时手势识别的函数
-   * @param  pointOne，第一个手指在屏幕上的变化方向
-   * @return 单指手势
-   */
-  /**
-    * @brief The function of gesture recognition when only a single finger touches
+    * @brief The function of gesture recognition for single finger touch
     * @param pointOne, the change direction of the first finger on the screen
-    * @return single finger gesture
+    * @return Single-finger gesture
     */
   eGesture_t getGestureOne(eDir_t pointOne);
   /**
-   * @brief 当有两个手指触摸时手势识别的函数
-   * @param  pointOne，第一个手指在屏幕上的变化方向
-   * @param  pointTwo，第二个手指在屏幕上的变化方向
-   * @return 两指手势
-   */
-  /**
-    * @brief gesture recognition function when two fingers touch
+    * @brief The function of gesture recognition for two-fingers touch
     * @param pointOne, the change direction of the first finger on the screen
     * @param pointTwo, the change direction of the second finger on the screen
-    * @return two finger gesture
+    * @return Two-fingers gesture
     */
   eGesture_t getGestureTwo(eDir_t pointOne,eDir_t pointTwo);
   /**
-   * @brief 当有三个手指触摸时手势识别的函数
-   * @param  pointOne，第一个手指在屏幕上的变化方向
-   * @param  pointTwo，第二个手指在屏幕上的变化方向
-   * @param  pointThree，第三个手指在屏幕上的变化方向
-   * @return 三指手势
-   */
-  /**
-    * @brief The function of gesture recognition when there are three fingers touching
+    * @brief The function of gesture recognition for three-fingers touch
     * @param pointOne, the change direction of the first finger on the screen
     * @param pointTwo, the change direction of the second finger on the screen
     * @param pointThree, the change direction of the third finger on the screen
-    * @return three finger gesture
+    * @return Three-fingers gesture
     */
   eGesture_t getGestureThree(eDir_t pointOne,eDir_t pointTwo,eDir_t pointThree);
   /**
-   * @brief 当有四个手指触摸时手势识别的函数
-   * @param  pointOne，第一个手指在屏幕上的变化方向
-   * @param  pointTwo，第二个手指在屏幕上的变化方向
-   * @param  pointThree，第三个手指在屏幕上的变化方向
-   * @param  pointFour，第四个手指在屏幕上的变化方向
-   * @return 四指手势
-   */
-  /**
-    * @brief The function of gesture recognition when there are four fingers touching
+    * @brief The function of gesture recognition for four-fingers touch
     * @param pointOne, the change direction of the first finger on the screen
     * @param pointTwo, the change direction of the second finger on the screen
     * @param pointThree, the change direction of the third finger on the screen
-    * @param pointFour, the changing direction of the fourth finger on the screen
-    * @return four finger gesture
+    * @param pointFour, the change direction of the fourth finger on the screen
+    * @return Four-fingers gesture
     */
   eGesture_t getGestureFour(eDir_t pointOne,eDir_t pointTwo,eDir_t pointThree,eDir_t pointFour);
   /**
-   * @brief 当有五个手指触摸时手势识别的函数
-   * @param  pointOne，第一个手指在屏幕上的变化方向
-   * @param  pointTwo，第二个手指在屏幕上的变化方向
-   * @param  pointThree，第三个手指在屏幕上的变化方向
-   * @param  pointFour，第四个手指在屏幕上的变化方向
-   * @param  pointFive，第五个手指在屏幕上的变化方向
-   * @return 五指手势
-   */
-  /**
-    * @brief The function of gesture recognition when five fingers touch
+    * @brief The function of gesture recognition for five-fingers touch
     * @param pointOne, the change direction of the first finger on the screen
     * @param pointTwo, the change direction of the second finger on the screen
     * @param pointThree, the change direction of the third finger on the screen
     * @param pointFour, the changing direction of the fourth finger on the screen
     * @param pointFive, the change direction of the fifth finger on the screen
-    * @return five finger gesture
+    * @return Five-fingers gesture
     */
   eGesture_t getGestureFive(eDir_t pointOne,eDir_t pointTwo,eDir_t pointThree,eDir_t pointFour,eDir_t pointFive);
   
-  sTouchMessage_t tpDev;//触摸点信息 //Touch point information
+  sTouchMessage_t tpDev;//Touch point information
 private:
   eDirection_t  directionX(uint16_t x1,uint16_t x2,uint16_t x3);
   eDirection_t  directionY(uint16_t y1,uint16_t y2,uint16_t y3);
